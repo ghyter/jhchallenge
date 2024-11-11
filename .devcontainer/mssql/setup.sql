4f2d3c39c2bce590942b7a646e6e5c9944e8fa81
Gabriel Hyter <ghyter@gmail.com>
2024-11-10 19:13:42 -0500

    updates to base

--- .devcontainer/mssql/setup.sql
+++ .devcontainer/mssql/setup.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE ApplicationDB;
 GO
+
+CREATE TABLE [dbo].[SubRedits]
+(
+  [Id] INT NOT NULL PRIMARY KEY,
+  [DisplayName] varchar(100),
+  [Reference] varchar(100),
+  [Description] varchar(255)
+)
+GO
